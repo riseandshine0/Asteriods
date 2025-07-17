fb79addbb3618fbbb902a72af7b0174db7f3637a
riseandshine0 <riseandshine@posteo.de>
2025-07-18 00:55:08 +0200

    CH2 L3:FPS

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +20,7 @@ def main():
             
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
