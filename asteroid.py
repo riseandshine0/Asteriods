f44e74949f6384dbb29e563e6cadd3476b66e611
riseandshine0 <riseandshine@posteo.de>
2025-07-18 23:53:32 +0200

    CH3 L1:Asteroids

---
+++ asteroid.py
@@ -0,0 +1,15 @@
+import pygame
+import circleshape
+from constants import *
+
+
+class Asteroid(circleshape.CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255,255,255), self.position, self.radius, width=2)
+    
+    def update(self, dt):
+        forward = pygame.Vector2(0, 1)
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -15,9 +17,13 @@ def main():
 
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
+    Asteroid.containers = (asteroids, updateable, drawable)
     Player.containers = (updateable, drawable)
+    AsteroidField.containers = (updateable)
 
+    asteroid_field = AsteroidField()
     player1 = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
     
     while True:
