6418106214c34dff17750c3e27c47aa1a57bd346
riseandshine0 <riseandshine@posteo.de>
2025-07-18 12:14:22 +0200

    CH2 L5:Draw Player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+import player
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player1 = player.player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
     dt = 0
 
 
@@ -19,6 +21,7 @@ def main():
                 return
             
         pygame.Surface.fill(screen, (0,0,0))
+        player1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
