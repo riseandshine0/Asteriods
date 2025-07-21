474f4e84a3f6b016df7a9ebbec1f5e659d2a80f6
riseandshine0 <riseandshine@posteo.de>
2025-07-21 21:43:04 +0200

    CH3 L4:rate limit

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(circleshape.CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.countdown = 0
         
     def draw(self, screen):
         pygame.draw.polygon(screen, (255,255,255), self.triangle(), 2)
@@ -22,6 +23,7 @@ class Player(circleshape.CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.countdown -= dt
 
         if keys[pygame.K_a]:
             self.rotate(- dt)
@@ -33,7 +35,7 @@ class Player(circleshape.CircleShape):
             self.move(- dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
-    
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
@@ -42,6 +44,8 @@ class Player(circleshape.CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
-        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        if self.countdown <= 0:    
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.countdown = PLAYER_SHOOT_COOLDOWN
         
